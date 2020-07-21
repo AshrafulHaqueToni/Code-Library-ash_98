803beb33b7a48dc18c566cada8d50f6f9aebb0cd
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-07-21 18:25:17 +0600

    Update euler totient function(me).cpp

--- number theorey+math/euler totient function(me).cpp
+++ number theorey+math/euler totient function(me).cpp
@@ -5,37 +5,21 @@ using namespace std;
 #define mod 1000000007
 #define mx 100000005
 
-bool is_composite[mx];
-vector<int>prime;
+
 int phi[mx];
 
-
-void sieve(int n)
+void seive(int n)
 {
-    fill(is_composite,is_composite+n,false);
-    phi[1]=1;
-
-    for(int i=2;i<=n;i++)
+    for(int i=2; i<=n; i++) phi[i]=i;
+    for(int i=2;i<=n;i+=2)
     {
-        if(!is_composite[i])
-        {
-            prime.push_back(i);
-            phi[i]=i-1;              ///i is prime
-        }
-
-        for(int j=0;j<prime.size() && i*prime[j]<=n;j++)
-        {
-            is_composite[i*prime[j]]=true;
-
-            if(i%prime[j]==0)
-            {
-                phi[i*prime[j]]=phi[i]*prime[j];   ///prime[j] divides i
-                break;
-            }
-            else
-            {
-                phi[i*prime[j]]=phi[i]*phi[prime[j]];  ///prime[j] do not divide i
-            }
+        phi[i]>>=1;
+    }
+    for(int i=3; i<=n; i+=2) {
+        if(phi[i]==i) {
+            phi[i]--;
+            for(int j=2*i; j<=n; j+=i)
+                phi[j]-=(phi[j]/i);
         }
     }
 }
