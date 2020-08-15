53f5f31b374dbc5148008331fd6e49bc60c04004
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-08-15 16:00:50 +0600

    Update optimize.cpp

--- stl+template/optimize.cpp
+++ stl+template/optimize.cpp
@@ -1,3 +1,6 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
 #pragma GCC optimization ("unroll-loops")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
