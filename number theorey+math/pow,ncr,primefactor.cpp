930d3b4bc0c3069f0785ffded2847ec1b2cb21cd
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2020-11-29 23:07:23 +0600

    Update pow,ncr,primefactor.cpp

--- number theorey+math/pow,ncr,primefactor.cpp
+++ number theorey+math/pow,ncr,primefactor.cpp
@@ -40,10 +40,11 @@ ll bigmod(ll e,ll x)
 void fact_cal()
 {
     fact[0]=1;
+    inv[0]=1;
     for(int i=1;i<=mx-3;i++)
     {
         fact[i]=(fact[i-1]*i)%mod;
     }
     inv[mx-3]=bigmod(fact[mx-3],mod-2);
-    for(int i=mx-4;i>=0;i--)inv[i]=(inv[i+1]*(i+1))%mod;
+    for(int i=mx-4;i>=1;i--)inv[i]=(inv[i+1]*(i+1))%mod;
 }
