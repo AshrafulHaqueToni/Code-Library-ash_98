3a5102081588f2562f1ace3a907990caacdafd28
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-03-08 19:34:27 +0600

    Create kuhn.cpp

---
+++ graph/Flow/kuhn.cpp
@@ -0,0 +1,55 @@
+#define mx 40005
+#define ll long long
+int n,m,ii,k;
+struct BPM
+{
+	bool Done[mx];
+    vector<int>g[mx];
+    int Left[mx],Right[mx];
+    void Done_Restore()
+    {
+      memset(Done,0,sizeof Done);
+    }
+    void addEdge(int u,int v)
+    {
+        g[u].push_back(v);
+    }
+    void init()
+    {
+        for(int i=0;i<mx;i++)g[i].clear();
+        memset(Done,0,sizeof Done);
+        memset(Left,-1,sizeof Left);
+        memset(Right,-1,sizeof Right);
+    }
+    bool Bipar_Matching(int u)
+    {
+        for(int &v: g[u])
+        {
+            if(Done[v]) continue;
+            Done[v] = true;
+            if(Right[v]==-1 || Bipar_Matching(Right[v]))
+            {
+                Left[u] = v;
+                Right[v] = u;
+                return true;
+            }
+        }
+        return false;
+    }
+    int Matching(int num)
+    {
+        memset(Left,-1,sizeof Left);
+        memset(Right,-1,sizeof Right);
+        int re = 0;
+        for(int i = 0; i < num; i++)
+        {
+            memset(Done,0,sizeof Done);
+            if(Bipar_Matching(i)) re++;
+        }
+        return re;
+    }
+ 
+ 
+};
+
+BPM bp;
