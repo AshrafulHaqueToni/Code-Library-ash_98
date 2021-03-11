7ad1d7ee14065342ada45805e7858367a68b355e
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-03-11 21:14:24 +0600

    Update DSU.cpp

--- graph/DSU.cpp
+++ graph/DSU.cpp
@@ -1,3 +1,5 @@
+int Size[mx];
+
 int Findparent(int x)
 {
     return (x==parent[x])?x:(parent[x]=Findparent(parent[x]));
@@ -5,10 +7,24 @@ int Findparent(int x)
 
 void Union(int x,int y)
 {
-    parent[Findparent(x)]=parent[Findparent(y)];
+    int px=Findparent(x);
+    int py=Findparent(y);
+    if(px==py)return;
+    if(Size[px]>Size[py])
+    {
+        Size[px]+=Size[py];
+        parent[py]=px;
+        parent[x]=parent[y]=px;
+    }
+    else
+    {
+        Size[py]+=Size[px];
+        parent[px]=py;
+        parent[x]=parent[y]=py;
+    }
 }
 
 void initialize()
 {
-    for(int i=0;i<=n;i++)parent[i]=i;
+    for(int i=0;i<=n;i++)parent[i]=i,Size[i]=1;
 }
