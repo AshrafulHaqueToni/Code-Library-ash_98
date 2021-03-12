437320f08dccf6b749a841ac653a8a34ccc1471b
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-03-13 00:23:35 +0600

    Update kuhn.cpp

--- graph/Flow/kuhn.cpp
+++ graph/Flow/kuhn.cpp
@@ -1,53 +1,49 @@
 /// for weight see lightoj 1150 solution
 
-#define mx 40005
+#define mx 10005
 #define ll long long
 int n,m,ii,k;
 struct BPM
 {
 	bool Done[mx];
     vector<int>g[mx];
-    int Left[mx],Right[mx];
-    void Done_Restore()
-    {
-      memset(Done,0,sizeof Done);
-    }
+    int macth[mx];
+
     void addEdge(int u,int v)
     {
         g[u].push_back(v);
     }
+
     void init()
     {
-        for(int i=0;i<mx;i++)g[i].clear();
-        memset(Done,0,sizeof Done);
-        memset(Left,-1,sizeof Left);
-        memset(Right,-1,sizeof Right);
+    	for(int i=0;i<mx;i++)g[i].clear();
     }
-    bool Bipar_Matching(int u)
+
+    bool Tem_Matching(int u)
     {
-        for(int &v: g[u])
+        for(int i=0;i<(int)g[u].size();i++)
         {
+        	int v=g[u][i];
             if(Done[v]) continue;
             Done[v] = true;
-            if(Right[v]==-1 || Bipar_Matching(Right[v]))
+            if(macth[v]==-1 || Tem_Matching(macth[v]))
             {
-                Left[u] = v;
-                Right[v] = u;
+                macth[v] = u;
                 return true;
             }
         }
         return false;
     }
-    int Matching(int num)
+    int Max_Matching(int num)
     {
 	// Be Careful with this section. when passin num.
-        memset(Left,-1,sizeof Left);
-        memset(Right,-1,sizeof Right);
+        memset(macth,-1,sizeof(macth));
         int re = 0;
-        for(int i = 1; i <=num; i++)
+        for(int i=1;i<=num;i++)
         {
-            memset(Done,0,sizeof Done);
-            if(Bipar_Matching(i)) re++;
+            memset(Done,false,sizeof(Done));
+            if(Tem_Matching(i)) re++;
+            
         }
         return re;
     }
