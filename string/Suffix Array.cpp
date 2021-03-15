80ea98134666bcbe582cd4452bc874de47beea0c
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-03-15 23:35:11 +0600

    Update Suffix Array.cpp

--- string/Suffix Array.cpp
+++ string/Suffix Array.cpp
@@ -107,6 +107,32 @@ pair<int,int> Patterntern_occurence(string Text ,string Pattern)
 
     return re;
 }
+/// this is for LCP from index i to index j.
+/// just run a query from min(Rank[i-1],Rank[j-1])+1 to max(Rank[i-1],Rank[j-1])
+int ST[mx][22];
+int Jump_LOG[mx];
+
+void Build_Sparse(int n)
+{
+	for(int i=1;i<=n;i++)ST[i][0]=LCP[i];
+
+	for(int i=2;i<=n;i++)Jump_LOG[i]=Jump_LOG[i-1]+!(i&(i-1));
+
+	for(int j=1;(1<<j)<=n;j++)
+	{
+		for(int i=1;(i+(1<<j)-1)<=n;i++)
+		{
+           ST[i][j]=min(ST[i][j-1],ST[i+(1<<(j-1))][j-1]);
+		}
+	}
+}
+
+int query(int i,int j)
+{
+	int boro_lav=Jump_LOG[j-i+1];
+	return min(ST[i][boro_lav],ST[j-(1<<boro_lav)+1][boro_lav]);
+}
+
 void solve()
 {
 	string s;
