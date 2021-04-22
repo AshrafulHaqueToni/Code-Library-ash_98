4bb22ccde6f37a97452b10bad3bfe22a715fb26a
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-04-22 23:51:17 +0600

    Update Gaussian Elimination.cpp

--- number theorey+math/Linear Equation -Matrix/Gaussian Elimination.cpp
+++ number theorey+math/Linear Equation -Matrix/Gaussian Elimination.cpp
@@ -1,17 +1,20 @@
 //Gaussian Elimination
 //format : (a[0]*x[0]+a[1]*x[1] ... a[m-1]*x[m-1]) % k = a[m], where 0 <= ai < k
 //number of solution : k^(number of free variable) = k^(n-rank)
+ll A[105][105];
+ll X[105];
+int Rank;
 
-int gcdExtended(int a, int b, int& x, int& y){
+ll gcdExtended(ll a, ll b, ll& x, ll& y){
     if(a==0) {x=0;y=1; return b;}
-    int x1,y1;
-    int gcd = gcdExtended(b%a,a,x1,y1);
+    ll x1,y1;
+    ll gcd = gcdExtended(b%a,a,x1,y1);
     x=y1-(b/a)*x1;
     y=x1;
     return gcd;
 }
 
-int modinverse(int x,int y) {int a,b; gcdExtended(x,y,a,b); return a;}
+ll modinverse(ll x,ll y) {ll a,b; gcdExtended(x,y,a,b); return a;}
 
 //n equations (n rows), m variables (m+1 columns)
 void Gauss(int n,int m,int k){
@@ -19,9 +22,9 @@ void Gauss(int n,int m,int k){
     for(r=0,c=0;r<n && c<m;c++){
         for(int i=r+1;i<n;i++) if(abs(A[i][c])>abs(A[r][c])) swap(A[i],A[r]);
         if(!A[r][c]) continue;
-        int s = modinverse(A[r][c],k);
+        ll s = modinverse(A[r][c],k);
         for(int i=r+1;i<n;i++) if(A[i][c]){
-            int w = (s*A[i][c])%k;
+            ll w = (s*A[i][c])%k;
             /* s bhag hobe r A[i][c] gun hobe*/
             for(int j=c;j<=m;j++){A[i][j]-=(A[r][j]*w)%k; A[i][j]%=k; if(A[i][j]<0) A[i][j]+=k;}
         }
@@ -32,10 +35,10 @@ void Gauss(int n,int m,int k){
 
     //Unique Solution for r variables
     for(int i=r-1;i>=0;i--){
-        x[i]=A[i][m];
-        for(int j=i+1;j<m;j++) {x[i]-=(A[i][j]*x[j])%k; x[i]%=k; if(x[i]<0) x[i]+=k;}
+        X[i]=A[i][m];
+        for(int j=i+1;j<m;j++) {X[i]-=(A[i][j]*X[j])%k; X[i]%=k; if(X[i]<0) X[i]+=k;}
 
-        int inv=modinverse(A[i][i],k);
-        x[i]=(x[i]*inv)%k; if(x[i]<0) x[i]+=k;
+        ll inv=modinverse(A[i][i],k);
+        X[i]=(X[i]*inv)%k; if(X[i]<0) X[i]+=k;
     }
 }
