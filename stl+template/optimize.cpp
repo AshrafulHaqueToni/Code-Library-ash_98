2db1a7bcbadc58ad1f2c2ce2d6be851c4019df8b
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-04-28 19:33:56 +0600

    Update optimize.cpp

--- stl+template/optimize.cpp
+++ stl+template/optimize.cpp
@@ -4,3 +4,16 @@
 #pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+
+/////
+#include <iostream>
+#include <chrono>
+#include <thread>
+ 
+int main()
+{
+    using namespace std::chrono_literals;
+
+    std::this_thread::sleep_for(-9999999999999ms);
+}
