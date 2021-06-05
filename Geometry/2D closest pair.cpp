490dd4f8c6cd7ae0a7baffbf67aae2ec725da6dc
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2021-06-05 20:59:27 +0600

    Update 2D closest pair.cpp

--- Geometry/2D closest pair.cpp
+++ Geometry/2D closest pair.cpp
@@ -1,16 +1,3 @@
-// Created by ash_98
- 
-#include<bits/stdc++.h>
-using namespace std;
- 
-#define mx 100005
-#define ll long long
-#define mod 1000000007
- 
-int ar[mx];
-char ch[mx];
-int m,ii,k;
-
 ll closest_pair(vector<pair<int,int>>point)
 {
     sort(point.begin(),point.end());
@@ -28,10 +15,8 @@ ll closest_pair(vector<pair<int,int>>point)
         }
         pair<int,int>a={point[i].second-sq_re,point[i].first};
         pair<int,int>b={point[i].second+sq_re,point[i].first};
-
         auto it1=event.lower_bound(a);
         auto it2=event.upper_bound(b);
-
         while(it1!=it2)
         {
             int dx=point[i].first-it1->second;
@@ -41,33 +26,5 @@ ll closest_pair(vector<pair<int,int>>point)
         }
         event.insert({point[i].second,point[i].first});
     }
-
     return re;
-
-}
-
-void solve()
-{
-    vector<pair<int,int>>point;
-    int n;
-    scanf("%d",&n);
-    int csum=0;
-    for(int i=1;i<=n;i++)
-    {
-        int x;
-        scanf("%d",&x);
-        csum+=x;
-        point.push_back({i,csum});
-    }
-    printf("%lld\n",closest_pair(point));
-
-
-}
- 
-int main()
-{
-    int t=1;
-   // scanf("%d",&t);
-    while(t--)solve();
-    return 0;
 }
