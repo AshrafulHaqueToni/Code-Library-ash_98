8ba56ec91c2436d219d254278fe343f5f309ebd9
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2022-01-25 15:37:10 +0600

    Update Square query (2D).cpp

--- data structure/Sparse_Table/Square query (2D).cpp
+++ data structure/Sparse_Table/Square query (2D).cpp
@@ -1,63 +1,42 @@
-#define mx 505
-#define MAX_logN 9
-
-int ar[mx][mx];
+#define mx 1005
+#define ll long long
+ 
+int dp[mx][mx],ar[mx][mx];
+int ST[mx][mx][10];
 int n,m,ii,k;
-int ST[mx][mx][MAX_logN][MAX_logN];  
-int Jump_LOG[505];
+int Jump_LOG[mx];
 
 void pre()
 {
-    for(int i=2;i<=500;i++)Jump_LOG[i]=Jump_LOG[i-1]+!(i&(i-1));
+    for(int i=2;i<=1000;i++)Jump_LOG[i]=Jump_LOG[i-1]+!(i&(i-1));
 }
 
-void Build_2D_Sparse()
+void Build_sparse_square(int N)
 {
-
-    for(int i=1;i<=n;i++)
+    for(int l=0;(1<<l)<=N;l++)
     {
-        for(int j=1;j<=m;j++)
+        for(int i=1;i+(1<<l)<N;i++)
         {
-            ST[i][j][0][0]=ar[i][j];
-        }
-    
-        for(int l=1;(1<<l)<=m;l++)
-        {
-            int pre=1<<(l-1);
-
-            for(int j=1;j+pre<=m;j++)
+            for(int j=1;j+(1<<l)<N;j++)
             {
-                ST[i][j][0][l]=max(ST[i][j][0][l-1],ST[i][j+pre][0][l-1]);
-            }
-        }
-    }
-
-    for(int l=1;(1<<l)<=n;l++)
-    {
-        int pre=1<<(l-1);
-
-        for(int i=1;i+pre<=n;i++)
-        {
-            for(int k=0;(1<<k)<=m;k++)
-            {
-                for(int j=1;j<=m;j++)
+                if(l==0)ST[i][j][l]=dp[i][j];
+                else
                 {
-                    ST[i][j][l][k]=max(ST[i][j][l-1][k],ST[i+pre][j][l-1][k]);
+                    int val1=max(ST[i][j][l-1],ST[i+(1<<(l-1))][j][l-1]);
+                    int val2=max(ST[i][j+(1<<(l-1))][l-1],ST[i+(1<<(l-1))][j+(1<<(l-1))][l-1]);
+                    ST[i][j][l]=max(val1,val2);
                 }
             }
         }
     }
 }
 
-int query(int i,int j,int p,int q)  /// two point
+int query(int i,int j,int l)
 {
-    int boro_jum1=Jump_LOG[p-i+1];
-    int boro_jum2=Jump_LOG[q-j+1];
-    int pre1=1<<boro_jum1;
-    int pre2=1<<boro_jum2;
-
-    int re1=max(ST[i][j][boro_jum1][boro_jum2],ST[i][q-pre2+1][boro_jum1][boro_jum2]);
-    int re2=max(ST[p-pre1+1][j][boro_jum1][boro_jum2],ST[p-pre1+1][q-pre2+1][boro_jum1][boro_jum2]);
-
+    int lg=Jump_LOG[l];
+    int add=1<<lg;
+    int re1=max(ST[i][j][lg],ST[i+l-add][j][lg]);
+    int re2=max(ST[i][j+l-add][lg],ST[i+l-add][j+l-add][lg]);
     return max(re1,re2);
+
 }
