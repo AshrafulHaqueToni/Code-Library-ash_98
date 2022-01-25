d0c9ae41c4888dbcef91e2e2c791d0652f78aea2
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2022-01-25 13:02:37 +0600

    Update BIT.cpp

--- data structure/BIT.cpp
+++ data structure/BIT.cpp
@@ -1,35 +1,46 @@
-struct BIT
-{
-    int Tree[N+5];
-    void init()
-    {
-    	memset(Tree,0,sizeof(Tree));
+//Tested : LightOJ 1164 - Horrible Queries
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const int MAXN = 1000005;
+
+ll BIT[2][MAXN];
+void update(int cs, int indx, ll val){
+    while(indx < MAXN){
+        BIT[cs][indx] += val;
+        indx += (indx & -indx);
     }
-    int query(int idx)
-    {
-        int re=0;
-        while(idx)
-        {
-            re+=Tree[idx];
-            idx-=idx&-idx;
+}
+
+ll sum(int cs, int indx){
+    ll ans = 0;
+    while(indx != 0) {
+        ans += BIT[cs][indx];
+        indx -= (indx & -indx);
+    }
+    return ans;
+}
+
+void updateRange(int l, int r, ll val){
+    update(0,l,val);       update(0,r+1,-val);
+    update(1,l,val*(l-1)); update(1,r+1,-val*r);
+}
+
+ll sumRange(int indx) {return sum(0,indx)*indx - sum(1,indx);}
+ll QueryRange(int l, int r) {return sumRange(r)-sumRange(l-1);}
+
+const int LOGN = 20;
+int LowerBound(int cs, ll v){
+    ll sum = 0;
+    int indx = 0;
+    for(int i = LOGN; i >= 0; i--){
+        int nPos = indx + (1<<i);
+        if(nPos < MAXN && sum + BIT[cs][nPos] < v){
+            sum += BIT[cs][nPos];
+            indx = nPos;
         }
-        return re;
     }
-    void update(int idx,int val)
-    {
-        while(idx<=N)
-        {
-            Tree[idx]+=val;
-            idx+=idx&-idx;
-        }
-    }
-    int Rquery(int l,int r)
-    {
-        return query(r)-query(l-1);
-    }
-    void Rupdate(int l,int r,int val)
-    {
-        update(l,val);
-        update(r+1,val*-1);
-    }
-};
+    //pos = maximal x such that Sum(x) < v
+    return indx + 1; //+1 for LowerBound
+}
