2be844b08e34b7bccc3a47050a442faec44f0188
Ashraful Haque Tani <48568933+AshrafulHaqueToni@users.noreply.github.com>
2023-02-02 16:14:14 +0600

    Create BASIS.cpp

---
+++ number theorey+math/Linear Equation -Matrix/BASIS.cpp
@@ -0,0 +1,42 @@
+struct BASIS {
+    int basis[d];
+    int sz;
+ 
+    void init() {
+        for(int i = 0; i < d; i++) basis[i] = 0;
+        sz = 0;
+    }
+ 
+    void insertVector(int mask) {
+        for (int i = d-1; i >= 0; i--) {
+            if (((mask>>i)&1) == 0) continue;
+ 
+            if (!basis[i]) {
+                basis[i] = mask;
+                ++sz;
+                return;
+            }
+            mask ^= basis[i];
+        }
+    }
+ 
+    void mergeBasis(const BASIS &from) {
+        for(int i = d-1; i >= 0; i--) {
+            if(!from.basis[i])
+                continue;
+ 
+            insertVector(from.basis[i]);
+        }
+    }
+ 
+    int findMax()  {
+        int ret = 0;
+        for(int i = d-1; i >= 0; i--) {
+            if(!basis[i] || (ret>>i & 1))
+                continue;
+ 
+            ret ^= basis[i];
+        }
+        return ret;
+    }
+}
